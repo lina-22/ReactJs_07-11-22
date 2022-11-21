b26de9bcbe9589be767af0354d1e98defb2408af
Fahinur Haque <lina-22@SuperMac.lan>
2022-11-21 10:30:00 +0100

    React HOOK applied

--- product-app/src/App.js
+++ product-app/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 
-import ProductDisplay from './product/ProductDisplay';
+import ProductDisplayVersionHook from './product/ProductDisplayVersionHook';
 // import ProductForm from './product/ProductForm';
 // import ProductTable from './product/ProductTable';
 function App() {
   return (
-    <ProductDisplay/>
+    // <ProductDisplay/>
+    <ProductDisplayVersionHook/>
     
   );
 }
