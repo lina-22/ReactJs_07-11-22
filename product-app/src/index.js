95ba9c5178161f6ecaad6b41d85748dd77690d82
Fahinur Haque <lina-22@SuperMac.local>
2022-11-25 02:12:59 +0100

    need to be clear about index.js routes and route

--- product-app/src/index.js
+++ product-app/src/index.js
@@ -5,23 +5,32 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import de bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, Routes} from 'react-router-dom';
-import ProductTable from './product/ProductTable';
-import ProductForm from './product/ProductForm';
+// import {BrowserRouter, Routes} from 'react-router-dom';
+// import ProductTable from './product/ProductTable';
+// import ProductForm from './product/ProductForm';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-   <BrowserRouter>
-    <Routes>
-      {/* <Routes path='/'elementv= {<ProductDisplay/>}/> */}
-      <Routes path='/' element= {<ProductTable products={[]}/>}/>
-      <Routes path='/product/add' element= {<ProductForm/>}/>
-      <Routes path='/product/update/{id}' element= {<ProductForm/>}/>
-    </Routes> 
-     <App />
-   </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
+
+// 12 until 17 lines or 20 until 33 lines
+// root.render(
+//   <React.StrictMode>
+//    <BrowserRouter>
+//     <Routes>
+    
+//       {/* <Routes path='/'elementv= {<ProductDisplay/>}/> */}
+//       <React.Fragment path='/' element= {<ProductTable products={[]}/>}/>
+//       <React.Fragment path='/product/add' element= {<ProductForm/>}/>
+//       <React.Fragment path='/product/update/{id}' element= {<ProductForm/>}/>
+//     </Routes> 
+//      <App />
+//    </BrowserRouter>
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
